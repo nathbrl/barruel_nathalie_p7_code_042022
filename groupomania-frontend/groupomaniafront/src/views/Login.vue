dd2530954207c61e43f5f903868623e8a5a2559e
nathbrl <70270233+nathbrl@users.noreply.github.com>
2022-05-30 18:02:07 +0200

    mise en route page login vue js
    mise en route page login vue js

---
+++ groupomania-frontend/groupomaniafront/src/views/Login.vue
@@ -0,0 +1,65 @@
+<!-- <template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style> -->
+<template>
+   <div class="login">
+         <h1>Bienvenue sur la page de connection</h1>
+         <form action="post" class="form">
+            <label for="mail">Votre e-mail {{ email }}</label>
+            <input type="mail" placeholder="e-mail" class="input-email">
+            <label for="password">Votre Mot de passe {{ password }}</label>
+            <input type="password" placeholder="mot de passe" class="input-password" v-model="password">
+            <div>
+               <button @click="click">Submit</button>
+            </div>
+         </form>
+   </div>
+</template>
+
+<script>
+
+export default {
+   data() {
+      return {
+         password: "mdp",
+         email: ""
+      }
+   },
+   methods: {
+      async click(event) {
+         event.preventDefault()
+         try {
+            debugger
+            const response = await fetch('http://localhost:3001/api/user/login', {method: 'POST', body:{ password: this.password, email: this.email}})
+            const donnees = await response.json();
+            console.log(donnees);
+         }
+         catch(error) {
+            console.log(error);
+         }
+      }
+   }
+};
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ groupomania-frontend/groupomaniafront/src/views/Signup.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+      <TheWelcome />
+      <form>
+         <label for="mail">Votre e-mail</label>
+         <input type="mail" placeholder="e-mail" class="input-email">
+         <label for="password">Votre Mot de passe</label>
+         <input type="password" placeholder="mot de passe" class="input-password">
+         <div>
+            <button>Submit</button>
+         </div>
+      </form>
+  </main>
+</template>
